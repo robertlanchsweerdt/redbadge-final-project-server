5eeec7243617c78ecb2fd3cbc971248c7ccf0b70
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-11-22 17:46:43 -0500

    code to db.js to allow for migration to heroku

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-})
+  ssl: process.env.ENVIRONMENT === 'production',
+});
 
 module.exports = db;

9c007ea1cf1937b2ec2eadbf5a397ffb7617ecb2
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-11-22 18:33:35 -0500

    comment out lines 5 to 10 in db.js

--- db.js
+++ db.js
@@ -2,13 +2,13 @@ const { Sequelize } = require('sequelize');
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // very important
-    },
-  },
-  //ssl: process.env.ENVIRONMENT === 'production',
+  // dialectOptions: {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false, // very important
+  //   },
+  // },
+  ssl: process.env.ENVIRONMENT === 'production',
 });
 
 module.exports = db;

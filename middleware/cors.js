e142982fceac6d63ba964ca896023a9ee1ed331b
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-11-22 20:48:10 -0500

    change to CORS

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,9 +1,15 @@
 const CorsMiddleware = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'POST, PUT, GET, DELETE, OPTIONS');
+  // res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Headers', 'POST, PUT, GET, DELETE, OPTIONS');
+  // res.header(
+  //   'Access-Control-Allow-Headers',
+  //   'Origin, X-Requested-with, Content-Type, Accept, Authorization'
+  // );
+  res.header('access-control-allow-origin', '*');
+  res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-with, Content-Type, Accept, Authorization'
+    'access-control-allow-headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   return next();
 };

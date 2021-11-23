6760628b9b0608f792fb5cf292fca5d6ca080883
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-11-22 21:09:53 -0500

    fix to CORS to resolve Heroku CORS error

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,10 +1,4 @@
 const CorsMiddleware = (req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', '*');
-  // res.header('Access-Control-Allow-Headers', 'POST, PUT, GET, DELETE, OPTIONS');
-  // res.header(
-  //   'Access-Control-Allow-Headers',
-  //   'Origin, X-Requested-with, Content-Type, Accept, Authorization'
-  // );
   res.header('access-control-allow-origin', '*');
   res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
   res.header(
